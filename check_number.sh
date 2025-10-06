6c371c8ddc833865e19fee2039904f83356e0e00
Sayad <sayad@example.com>
2025-10-06 20:09:00 +0000

    update

--- check_number.sh
+++ check_number.sh
@@ -1,9 +1,10 @@
-
 #!/bin/bash
 echo "enter a number:"
 read num
-if [ $num -gt 10 ]; then
-echo "your number is greater than 10."
+if [ $num -gt 20 ]; then
+echo "your number is greater than 20."
+elif [ $num -gt 10 ]; then
+echo "your number is greater than  10  but 20 or less."
 else
 echo "your number is 10 or less."
 fi
